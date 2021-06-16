0a6319a730fd16a52890f1dfdf52159f9ccd0899
christianp <porsc.chr@gmail.com>
2021-06-16 12:28:10 +0200

    Add empty CardSearchResult Page and routes for the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import LandingPage from "./pages/LandingPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CardSearchResult from "./pages/CardSearchResult";
 
 function App() {
   return (
-    <div>
-        <LandingPage />
-    </div>
+      <Router>
+        <Switch>
+          <Route path={'/'} exact>
+                <LandingPage />
+          </Route>
+          <Route path={'/searchCards'} exact>
+                <CardSearchResult />
+          </Route>
+        </Switch>
+     </Router>
   );
 }
 
---
+++ frontend/src/pages/CardSearchResult.js
@@ -0,0 +1,7 @@
+export default function CardSearchResult(){
+    return(
+        <div>
+            cardsearchresult
+        </div>
+    )
+}
