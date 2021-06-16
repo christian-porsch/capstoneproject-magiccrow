5a1ac82a55546aa2f857279aee9e6f74f883cbf0
christianp <porsc.chr@gmail.com>
2021-06-16 11:52:52 +0200

    Refactor CardSearchController, add CardSearchService, add CardSearch logic to LandingPage

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,16 +1,28 @@
 import styled from 'styled-components/macro'
+import {useState} from "react";
+import axios from "axios";
 
 export default function LandingPage(){
 
-    const handleChange = () => {
-        console.log('searching for card')
+    const [cardName, setCardName] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.get('/api/cards?cardName=' + cardName)
+            .then(response => response.data)
+            .then ((data) => console.log(data))
     }
 
     return(
         <Wrapper>
             <h2>Welcome to MagicCrow</h2>
-            <form>
-                <input type='text' placeholder='search for a card' onChange={handleChange}/>
+            <form onSubmit = {handleSubmit} >
+                <input
+                    type ='text'
+                    placeholder ='search for a card'
+                    value ={cardName}
+                    onChange = {event => setCardName(event.target.value)}
+                    />
                 <button>
                     search
                 </button>
