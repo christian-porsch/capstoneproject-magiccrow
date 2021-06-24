449e60b89b6f2aff6ef4e07998a10abd0d9a26cb
christianp <porsc.chr@gmail.com>
2021-06-24 09:59:22 +0200

    Add useCardsInPile and render it in CardCollectionPage, but there is an issue with multiple cards of same id

---
+++ frontend/src/hooks/useCardsInPile.js
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export default function useCardsInPile(){
+
+    const [cardsInPile, setCardsInPile] = useState();
+
+    useEffect(() => {
+        axios
+            .get('/api/cardsInPile/60d2f120c76f8707f38e9a99')
+            .then(response => response.data)
+            .then(setCardsInPile)
+    }, [])
+
+    return {
+        cardsInPile
+    }
+
+}
--- frontend/src/pages/CardCollectionPage.js
+++ frontend/src/pages/CardCollectionPage.js
@@ -1,8 +1,27 @@
+import useCardsInPile from "../hooks/useCardsInPile";
+import styled from "styled-components/macro";
 
-export default function CardCollectionPage(){
-    return(
+export default function CardCollectionPage() {
+
+    const {cardsInPile} = useCardsInPile();
+
+    return (
         <div>
             <h2>welcome to your collection</h2>
+            {cardsInPile && <section>
+                {cardsInPile.map(cardInPile => (
+                    <ImageAppearance key={cardInPile.id}src={cardInPile.image_uris?.normal} alt={cardInPile.name} />))}
+            </section>}
         </div>
     )
 }
+
+const ImageAppearance = styled.img`
+    
+        justify-content: space-evenly;
+        margin: 5px;
+        width: 150px;
+        height: auto;
+        border-radius: 7%;
+    
+    `
