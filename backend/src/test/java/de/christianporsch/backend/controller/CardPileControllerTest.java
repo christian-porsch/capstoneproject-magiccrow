489c212d9f243eeda0ef7842d69561c2fd058d95
christianp <porsc.chr@gmail.com>
2021-07-01 12:36:18 +0200

    Add all missing integration tests to CardPileControllerTest

--- backend/src/test/java/de/christianporsch/backend/controller/CardPileControllerTest.java
+++ backend/src/test/java/de/christianporsch/backend/controller/CardPileControllerTest.java
@@ -1,10 +1,11 @@
 package de.christianporsch.backend.controller;
 
-import de.christianporsch.backend.model.CardImage;
-import de.christianporsch.backend.model.MagicCardInPile;
-import de.christianporsch.backend.model.User;
+import de.christianporsch.backend.model.*;
+import de.christianporsch.backend.model.dto.MagicCardDto;
 import de.christianporsch.backend.repository.MagicCardInPileRepository;
+import de.christianporsch.backend.repository.MagicCardRepository;
 import de.christianporsch.backend.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,13 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,6 +31,9 @@ class CardPileControllerTest {
     private int port;
 
     @Autowired
+    private MagicCardRepository magicCardRepository;
+
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
@@ -35,6 +41,15 @@ class CardPileControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @BeforeEach
+    public void clearRepositories(){
+        magicCardRepository.deleteAll();
+        magicCardInPileRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+
 
     @Test
     @DisplayName("Method should return pile of cards from a user found by user id")
@@ -54,23 +69,135 @@ class CardPileControllerTest {
         // Then
 
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
-        assertThat(response.getBody(),arrayContainingInAnyOrder(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+        assertThat(response.getBody(), arrayContainingInAnyOrder(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
                 new CardImage("tarmoHighresImg"), "some set", 1, false)));
     }
 
     @Test
-    void findMagicCardInPileById() {
+    @DisplayName("Method should find magic cards in pile by Id from Db")
+    public void findMagicCardInPileById() {
+
+        // Given
+
+        magicCardInPileRepository.save(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 1, false));
+
+        // When
+
+        ResponseEntity<MagicCardInPile> response = restTemplate.getForEntity("http://localhost:" + port + "/api/cardsInPile/specificCardInPile/1", MagicCardInPile.class);
+
+        // Then
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody(), is(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 1, false)));
+
+
     }
 
     @Test
-    void addMagicCardToPile() {
+    @DisplayName("Method should add whole magic card to pile of user")
+    public void addMagicCardToPile() {
+
+        // Given
+
+        userRepository.save(new User("60d2f120c76f8707f38e9a99", "christian", new ArrayList<>()));
+        magicCardRepository.save(new MagicCard("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", new Price(100, 25, 17)));
+        MagicCardDto magicCardToAdd = new MagicCardDto("1");
+
+
+        // When
+
+        ResponseEntity<MagicCardInPile> response = restTemplate.postForEntity("http://localhost:" + port + "/api/cardsInPile", magicCardToAdd, MagicCardInPile.class);
+
+        // Then
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody(), is(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 1, false)));
+
+        Optional<MagicCardInPile> pileOfCards = magicCardInPileRepository.findMagicCardInPileById("1");
+        assertThat(pileOfCards.get(), is(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 1, false)));
+
     }
 
     @Test
-    void decreaseMagicCardInPileAmount() {
+    @DisplayName("If magic card is already in collection method should increase amount by 1")
+    public void addMagicCardToPileIncreaseAmount() {
+
+        // Given
+
+        userRepository.save(new User("60d2f120c76f8707f38e9a99", "christian", List.of(
+                new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                        new CardImage("tarmoHighresImg"), "some set", 10, false))));
+        magicCardRepository.save(new MagicCard("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", new Price(100, 25, 17)));
+        magicCardInPileRepository.save(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 10, false));
+
+        MagicCardDto magicCardToAdd = new MagicCardDto("1");
+
+
+        // When
+
+        ResponseEntity<MagicCardInPile> response = restTemplate.postForEntity("http://localhost:" + port + "/api/cardsInPile", magicCardToAdd, MagicCardInPile.class);
+
+        // Then
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody(), is(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 11, false)));
+
     }
 
     @Test
-    void deleteMagicCardInPileById() {
+    @DisplayName("Method should decrease the amount of a magic card if it is present in users pile")
+    public void decreaseMagicCardInPileAmount() {
+
+        // Given
+
+        userRepository.save(new User("60d2f120c76f8707f38e9a99", "christian", List.of(
+                new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                        new CardImage("tarmoHighresImg"), "some set", 10, false))));
+        magicCardRepository.save(new MagicCard("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", new Price(100, 25, 17)));
+        magicCardInPileRepository.save(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 10, false));
+
+        // When
+
+        restTemplate.put("http://localhost:" + port + "/api/cardsInPile/updateCardInPile/1", MagicCardInPile.class);
+
+        // Then
+
+        Optional<MagicCardInPile> pileOfCards = magicCardInPileRepository.findMagicCardInPileById("1");
+        assertThat(pileOfCards.get(), is(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 9, false)));
+
+    }
+
+    @Test
+    @DisplayName("Method should delete magic cards from pile from db")
+    public void deleteMagicCardInPileById() {
+
+        // Given
+
+        userRepository.save(new User("60d2f120c76f8707f38e9a99", "christian", List.of(
+                new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                        new CardImage("tarmoHighresImg"), "some set", 10, false))));
+        magicCardInPileRepository.save(new MagicCardInPile("1", "Tarmogoyf", "some oracle text about tarmo",
+                new CardImage("tarmoHighresImg"), "some set", 10, false));
+
+        // When
+
+        restTemplate.delete("http://localhost:" + port + "/api/cardsInPile/1");
+
+        // Then
+
+        Optional<MagicCardInPile> pileOfCards = magicCardInPileRepository.findMagicCardInPileById("1");
+        assertTrue(pileOfCards.isEmpty());
+
     }
 }
