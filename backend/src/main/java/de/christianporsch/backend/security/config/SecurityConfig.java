e1d31f42a247236cf4de6ab5801efafc0dc1bfaa
christianp <porsc.chr@gmail.com>
2021-07-13 10:20:07 +0200

    Fix security config

--- backend/src/main/java/de/christianporsch/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/de/christianporsch/backend/security/config/SecurityConfig.java
@@ -39,8 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/login").permitAll()
-                .antMatchers("/**").authenticated()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/api/**").authenticated()
                 .and().addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
